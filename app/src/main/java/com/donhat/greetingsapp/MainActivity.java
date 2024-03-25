2dab053dfd44481423a5362b9b74c3b5042e3c3c
Do Nhat <dmnhat.pt@gmail.com>
2024-03-25 11:21:48 +0700

    Initial commit

---
+++ app/src/main/java/com/donhat/greetingsapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.donhat.greetingsapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editText = findViewById(R.id.editText);
+        Button myBtn = findViewById(R.id.btn);
+
+
+        myBtn.setOnClickListener(v -> {
+
+            String inputName = editText.getText().toString().trim();
+
+            Toast.makeText(
+                    MainActivity.this,
+                    "Welcome " + inputName + " to our App",
+                    Toast.LENGTH_LONG).show();
+        });
+
+    }
+}
